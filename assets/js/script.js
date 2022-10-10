37c46cf05b7a0d90f49c3909cbcedfd9eb6b4f3a
isaias <104476799+mrza1127@users.noreply.github.com>
2022-10-10 15:35:25 -0500

    css styling and some js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// declaring all the screens as constants
+const startQuizScreenEl = document.getElementById("start-quiz-screen");
+const quizScreenEl = document.getElementById("quiz-screen");
+const resultScreenEl = document.getElementById("result-screen");
+const completeScreen = document.getElementById("complete-screen");
+const highScoresScreenEl = document.getElementById("high-scores-screen");
